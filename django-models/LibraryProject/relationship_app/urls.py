ee68ea60fc3d6cc0b183c7fd15cac5221c847891
Ol4k7 <tolulopetheophilus@gmail.com>
2025-09-09 19:07:07 -0700

    Fix URLs to link custom authentication views correctly

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import CustomLoginView, CustomLogoutView, RegisterView, home
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("register/", RegisterView.as_view(), name="register"),
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication views using the custom views
+    path("login/", views.CustomLoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
