fd98a7a7daa10925e5bdf36b2eaddbf42d828580
Ol4k7 <tolulopetheophilus@gmail.com>
2025-09-09 18:43:28 -0700

    Implement user authentication views and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Library, Book
+from django.views import View
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Library, Book
+
+
+# --- Authentication Views ---
+def home(request):
+    return render(request, "relationship_app/home.html")
+
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
+
+    # Allow GET requests to log out (instead of only POST)
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "relationship_app/register.html", {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+        return render(request, "relationship_app/register.html", {"form": form})
+
+
+# --- Book & Library Views ---
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 # Class-based view for library detail
 class LibraryDetailView(DetailView):
