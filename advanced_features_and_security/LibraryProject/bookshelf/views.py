18e173da24b679f334bc6c8c954f9f84d58267d9
Ol4k7 <tolulopetheophilus@gmail.com>
2025-09-11 15:45:58 -0700

    importing forms module

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book, Author
+from .forms import ExampleForm
 
 # Home page (optional)
 def home(request):
