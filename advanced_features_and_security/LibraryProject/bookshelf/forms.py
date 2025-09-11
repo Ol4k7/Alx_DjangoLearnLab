70db8ab343f3f8851f62b000b12953a895c2a378
Ol4k7 <tolulopetheophilus@gmail.com>
2025-09-11 15:31:40 -0700

    forms update

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,14 @@
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
+from .models import Book, Author
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
+
+class AuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ['name']
