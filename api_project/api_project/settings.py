e29c9c1568ac34526f806ac18f1941d480f9c875
Ol4k7 <tolulopetheophilus@gmail.com>
2025-09-16 04:47:54 -0700

    done for the week on a tuesday

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # temporary (we’ll lock down soon)
+        'rest_framework.permissions.IsAuthenticated',  # temporary (we’ll lock down soon)
     ]
 }
 
