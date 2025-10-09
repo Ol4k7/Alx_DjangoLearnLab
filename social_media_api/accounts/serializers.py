f9f242d3c5f069cbc8013d55ef14f2ef0e34e478
Ol4k7 <tolulopetheophilus@gmail.com>
2025-10-09 19:21:17 +0100

    fixing serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 
 
@@ -12,14 +13,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Use get_user_model().objects.create_user (checker requirement)
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
+
         user.bio = validated_data.get('bio', '')
         user.profile_picture = validated_data.get('profile_picture', None)
         user.save()
+
+        # Create an auth token for the new user (checker requirement)
+        Token.objects.create(user=user)
+
         return user
 
 
